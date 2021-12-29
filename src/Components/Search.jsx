12d17c86ad4499ff9418b5d3729d8991b28cb746
RobertaMatera <materaroberta11@gmail.com>
2021-12-29 18:14:38 +0100

    search component fixed + first dropdown menu

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -11,16 +11,24 @@ export default class Search extends Component {
         <span className="d-flex flex-row bg-button rounded-border-32">
           <Dropdown>
             <Dropdown.Toggle className="bg-button rounded-border-32">
-              <div className="d-flex flex-column text-left my-2">
+              <div className="d-flex flex-column text-left my-2 ml-3 mr-5">
                 <p className="my-0"> Location </p>
                 <p className="my-0 disabled"> Where are you going? </p>
               </div>
             </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            <Dropdown.Menu id="flexible-menu" className="bg-button rounded-border-32 p-4 mt-3">
+              <p>
+                <b>GO ANYWHERE, ANYTIME</b>
+              </p>
+
+              <Button
+                id="flexible-btn"
+                className="bg-button rounded-border d-flex align-items-center mt-3 px-4"
+              >
+                {" "}
+                I'm flexible <i className="bi bi-caret-right ml-auto"></i>
+              </Button>
             </Dropdown.Menu>
           </Dropdown>
 
@@ -28,24 +36,20 @@ export default class Search extends Component {
 
           <Dropdown>
             <Dropdown.Toggle className="bg-button rounded-border-32">
-              <div className="d-flex flex-column text-left my-2">
+              <div className="d-flex flex-column text-left my-2 ml-3 mr-5">
                 <p className="my-0"> Check in </p>
                 <p className="my-0 disabled"> Add dates </p>
               </div>
             </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
+            <Dropdown.Menu></Dropdown.Menu>
           </Dropdown>
 
           <span className="vl"></span>
 
           <Dropdown>
             <Dropdown.Toggle className="bg-button rounded-border-32">
-              <div className="d-flex flex-column text-left my-2">
+              <div className="d-flex flex-column text-left my-2 ml-3 mr-5">
                 <p className="my-0"> Check out </p>
                 <p className="my-0 disabled"> Add dates </p>
               </div>
@@ -62,11 +66,11 @@ export default class Search extends Component {
 
           <Dropdown>
             <Dropdown.Toggle className="d-flex flex-row bg-button rounded-border-32">
-              <div className="d-flex flex-column text-left my-2">
+              <div className="d-flex flex-column text-left my-2 ml-3 mr-5">
                 <p className="my-0"> Guests </p>
                 <p className="my-0 disabled"> Add guests </p>
               </div>
-              <i className="bi bi-search d-flex align-self-center my-auto ml-4 red-search"></i>
+              <i id="search-i" className="bi bi-search d-flex align-self-center my-auto ml-4 red-search"></i>
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
