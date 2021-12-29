2884d6cf94ef1383a13cbdea24d003de27c14b54
RobertaMatera <materaroberta11@gmail.com>
2021-12-29 17:04:26 +0100

    search component base settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import './Style/MyStyle.css';
-import './Style/MyNav.css';
-import './Style/MySmallNavStyle.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import "./Style/MyStyle.css";
+import "./Style/MyNav.css";
+import "./Style/MySmallNavStyle.css";
+import "./Style/MySearchStyle.css";
 
-import MyNav from './Components/MyNav';
-import MySmallNav from './Components/MySmallNav';
+import MyNav from "./Components/MyNav";
+import MySmallNav from "./Components/MySmallNav";
+import Search from "./Components/Search";
 
 function App() {
   return (
-    <section id="nav">
-      <MyNav/>
-      <MySmallNav/>
-    </section>
+    <>
+      <section id="nav">
+        <MyNav />
+        <MySmallNav />
+      </section>
+      <section id="search" className="bg-black py-3 d-none d-md-flex justify-content-center">
+        <Search />
+      </section>
+    </>
   );
 }
 
---
+++ src/Components/Search.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
+
+export default class Search extends Component {
+  render() {
+    return (
+      <div>
+        <span className="d-flex flex-row bg-button rounded-border-32">
+          <Dropdown>
+            <Dropdown.Toggle className="bg-button rounded-border-32">
+              <div className="d-flex flex-column text-left my-2">
+                <p className="my-0"> Location </p>
+                <p className="my-0 disabled"> Where are you going? </p>
+              </div>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <span className="vl"></span>
+
+          <Dropdown>
+            <Dropdown.Toggle className="bg-button rounded-border-32">
+              <div className="d-flex flex-column text-left my-2">
+                <p className="my-0"> Check in </p>
+                <p className="my-0 disabled"> Add dates </p>
+              </div>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <span className="vl"></span>
+
+          <Dropdown>
+            <Dropdown.Toggle className="bg-button rounded-border-32">
+              <div className="d-flex flex-column text-left my-2">
+                <p className="my-0"> Check out </p>
+                <p className="my-0 disabled"> Add dates </p>
+              </div>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <span className="vl"></span>
+
+          <Dropdown>
+            <Dropdown.Toggle className="d-flex flex-row bg-button rounded-border-32">
+              <div className="d-flex flex-column text-left my-2">
+                <p className="my-0"> Guests </p>
+                <p className="my-0 disabled"> Add guests </p>
+              </div>
+              <i className="bi bi-search d-flex align-self-center my-auto mx-4 red-search"></i>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </span>
+      </div>
+    );
+  }
+}
